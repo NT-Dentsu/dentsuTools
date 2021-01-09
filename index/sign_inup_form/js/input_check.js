cd7e82c723abb5e2c00453b84722d70040599109
bot810 <71932961+bot810@users.noreply.github.com>
2021-01-09 23:23:25 +0900

    とりあえずのディレクトリ構成

---
+++ index/sign_inup_form/js/input_check.js
@@ -0,0 +1,21 @@
+$(function() {
+  value_check();
+  $('form').submit(function() {
+    if ($('.is-invalid').length != 0) {
+      alert('半角英数字ハイフン，20文字以内で入力してください');
+      return false;
+    }
+  });
+});
+
+function value_check() {
+  $('input').on("keyup blur", function() {
+    if( $(this).val().length >= 6 && $(this).val().length <= 20) {
+      if ($(this).val().match(/^[0-9a-z\-]+$/)) {
+        $(this).removeClass('is-invalid');
+        return;
+      }
+    }
+    $(this).addClass('is-invalid');
+  });
+}
