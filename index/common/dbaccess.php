90f0e9187b3987d46c901a9ea082f638f28b1c14
YUUKI <YUUKI>
2021-03-12 20:58:04 +0900

    close #14 SQL実行処理の修正

--- index/common/dbaccess.php
+++ index/common/dbaccess.php
@@ -1,6 +1,6 @@
 <?php
 # 制作者:KARASU-2000
-# 更新日:2021/3/6
+# 更新日:2021/3/12
 # 機能:DBアクセスクラス
 Class DBAccess{
     # 定数宣言
@@ -14,9 +14,9 @@ Class DBAccess{
     const PASSWORD = 'DentsuTool';
 
     # 制作者:KARASU-2000
-    # 更新日:2021/3/6
+    # 更新日:2021/3/12
     # 機能:DBアクセスの用のインスタンスを生成する
-    private function connectDB(){
+    public function connectDB(){
         try{
             # 接続文字列を生成
             $dns = 'mysql:dbname='.self::DBNAME.';host='.self::HOST_NAME;
@@ -38,12 +38,12 @@ Class DBAccess{
     }
 
     # 制作者:KARASU-2000
-    # 更新日:2021/3/6
+    # 更新日:2021/3/12
     # 機能:受け取ったクエリを実行する(SELECT)
-    public function getTableSQL($sql){
+    public function getTableSQL($pdo, $sql){
         try{
             # クエリ実行
-            $dataTable = $this->connectDB()->query($sql);
+            $dataTable = $pdo->query($sql);
             # テーブルを返却
             return $dataTable;
         }
@@ -54,12 +54,12 @@ Class DBAccess{
     }
 
     # 制作者:KARASU-2000
-    # 更新日:2021/3/6
+    # 更新日:2021/3/12
     # 機能:受け取ったクエリを実行する(INSERT, UPDATE, DELETE)
-    public function executeSQL($sql){
+    public function executeSQL($pdo, $sql){
         try{
             # クエリ実行
-            $result = $this->connectDB()->exec($sql);
+            $result = $pdo->exec($sql);
             # 操作したレコード件数を返却
             return $result;
         }
