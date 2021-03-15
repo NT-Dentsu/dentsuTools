3c017ed0bf602b811813862e9e705420d89388d1
bot810 <kutsuhimo197@gmail.com>
2021-03-16 00:05:51 +0900

    index.htmlのメニューバーにpanel_customize.htmlへのリンクを設定 panel_customize.htmlをindex.htmlをもとに外枠だけ制作 bootstrap.bundle.jsとかいうファイル追加．長いから適当に見て

---
+++ index/panel_customize/js/div_control.js
@@ -0,0 +1,30 @@
+/*
+div要素の追加，変更をする．
+*/
+
+//div要素の追加先id取得
+let id = $('#container');
+
+
+let name = 'カッコイイよね';
+let num = 0;
+let text;
+let className = ['panel m left', 'panel m right', 'panel s', 'panel m top',
+                'panel l leftTop', 'panel l rightTop', 'panel m top', 'panel m bottom',
+                'panel l leftBottom', 'panel l rightBottom', 'panel m bottom', 'panel s',
+                'panel s', 'panel m left', 'panel m right', 'panel s'];
+
+
+for(num=0; num<16; num++){
+    text = `
+    <div class="panel-wrap">
+        <div class="${className[num]}">
+            <a href="https://ja.wikipedia.org/wiki/%E3%82%B4%E3%83%AA%E3%83%A9">
+                <img src="images/image02.jpg" alt="">
+            </a>
+            <p>${name} ${num}</p>
+        </div>
+    </div>
+    `;
+    id.append(text);
+}
