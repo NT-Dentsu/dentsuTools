c051c03fb0b9f37d503dca7698dae64051cf778f
towa1204 <fdsfgjkal@gmail.com>
2021-03-16 19:05:05 +0900

    postメソッドで受け取ったデータを処理するphpファイルの追加

---
+++ index/sign_inup_form/php/ajax_getData.php
@@ -0,0 +1,15 @@
+<?php
+    // デバッグ用の処理
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'On');
+    //ヘッダー情報の明記。必須。
+    header("Content-Type: application/json; charset=UTF-8");
+    $uid = filter_input(INPUT_POST, 'uid');
+    $password = filter_input(INPUT_POST, 'password');
+    // jsの方にデータ渡して、動作確認をする
+    $array_lists = [
+        'uidLength' => $uid,
+        'passwordLength' => $password
+    ];
+    echo json_encode($array_lists);
+?>
