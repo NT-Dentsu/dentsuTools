6113a0da7146d0b9fc53d899c6ff3b895b6894a8
towa1204 <fdsfgjkal@gmail.com>
2021-03-17 02:26:15 +0900

    サインイン、サインアップの処理の切り分け

--- index/sign_inup_form/js/ajax_postData.js
+++ index/sign_inup_form/js/ajax_postData.js
@@ -1,18 +1,26 @@
+// signin, signup どちらもこれを利用する
+// クリックしたボタンのidによって呼び出すphpファイルのパスを切り替える
 $(function(){
-    $('#signin_button').click(function() {
+    $('form button').click(function() {
+        let path;
+        if (this.id === 'signin_button') {
+            path = './php/match_data.php'
+        } else {
+            path = './php/insert_data.php'
+        }
         // 入力値チェックの基準を満たしているとき
         if ($('.is-invalid').length == 0) {
             $.post({
                 // 相対パスはこのファイルを読み込むファイルから見たパスになる
-                url: './php/ajax_getData.php',
+                url: path,
                 data:{
-                    uid: $('input[name=uid]').val(),
-                    password: $('input[name=password').val()
+                    uid: $('input[type=text]').val(),
+                    password: $('input[type=password').val()
                 },
                 dataType: 'json', //必須。json形式で返すように設定
             }).done(function(data){
                 //連想配列のプロパティがそのままjsonオブジェクトのプロパティとなっている
-                alert('uidの長さ：' + data.uidLength + ' passwordの長さ：' + data.passwordLength);
+                alert('uidの値：' + data.uidLength + ' passwordの値：' + data.passwordLength);
             }).fail(function(XMLHttpRequest, textStatus, errorThrown){
                 alert(errorThrown);
             })
