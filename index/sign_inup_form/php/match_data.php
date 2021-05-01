a48b63be8752cf30164e5296554cc40d4fed06eb
towa1204 <fdsfgjkal@gmail.com>
2021-05-01 14:53:57 +0900

    パスワードの照合処理の変更

--- index/sign_inup_form/php/match_data.php
+++ index/sign_inup_form/php/match_data.php
@@ -1,27 +1,28 @@
 <?php
     // デバッグ用の処理
-    // error_reporting(E_ALL);
-    // ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'On');
     //ヘッダー情報の明記。必須。
     header("Content-Type: application/json; charset=UTF-8");
     $uid = filter_input(INPUT_POST, 'uid');
-    $password = filter_input(INPUT_POST, 'password');
+    $input_password = filter_input(INPUT_POST, 'password');
 
     // データベース共通クラスの読み込み
     require_once('../../common/dbaccess.php');
+    // パスワードハッシュ値生成に使う関数の読み込み
+    require_once('../../common/password_hash.php');
     require_once('./input_check.php');
 
     // 入力値チェックを行う
-    if (input_check($uid) && input_check($password)) {
+    if (input_check($uid) && input_check($input_password)) {
         $db = new DBAccess();
         // データベース接続開始
         $pdo = $db->connectDB();
 
         // プレースホルダを用いたSQL文を生成（プリペアドステートメント）
-        $sql = "SELECT COUNT(*) FROM m_user WHERE user_id=:user_id AND password=:password";
-
+        $sql = "SELECT password_hash FROM m_user WHERE user_id=:user_id";
         // プレースホルダに対応する値を設定
-        $parameter = array(':user_id'=>$uid, ':password'=>$password);
+        $parameter = array(':user_id'=>$uid);
 
         // 照合一致:true, 照合不一致:falseを代入
         $result = false;
@@ -29,9 +30,10 @@
         try {
             // SQL文を実行
             $stmt = $db->executePrepareSQL($pdo, $sql, $parameter);
-            // 1のとき照合成功，0のとき照合失敗
-            $count = $stmt->fetchColumn();
-            if ($count === 1) {
+            // SQL文にあてはまる最初の値を取得（同じuser_idをもつカラムがないことを前提）
+            $value = $stmt->fetchColumn();
+            if ($value != false && verifyPasswordHash($input_password, $value)) {
+                // user_idが一致　かつ　パスワードが一致　のとき
                 $result = true;
                 $massage = 'ログイン成功しました';
             }
