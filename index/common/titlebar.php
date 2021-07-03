a6684e7e0a7702b5c106d1e1668520338edf4590
towa1204 <fdsfgjkal@gmail.com>
2021-07-03 19:31:17 +0900

    モジュールの追加

---
+++ index/common/titlebar.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">NT-dentsu</a>
+
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- 検索バー -->
+    <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
+
+    <!-- こ↑こ↓，サインイン処理 -->
+    <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+            <a class="nav-link" href="./sign_inup_form/signin.php">Sign in</a>
+        </li>
+    </ul>
+</nav>
