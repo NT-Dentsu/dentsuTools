5ae7780eaf6163bb02504fd038a1eb4691c7bb9c
bot810 <kutsuhimo197@gmail.com>
2021-09-11 13:59:20 +0900

    ダブルクリック対応

--- index/panel_customize/js/panel_priset_layout.js
+++ index/panel_customize/js/panel_priset_layout.js
@@ -27,23 +27,114 @@ let data04 = new Array();
 
 // イベント設定(暫定)
 // 参考サイト：https://qiita.com/hththt/items/aefbcc6eb191588dadff#%E3%82%AD%E3%83%A3%E3%83%97%E3%83%81%E3%83%A3%E3%83%AA%E3%83%B3%E3%82%B0%E3%83%95%E3%82%A7%E3%83%BC%E3%82%BA%E3%81%A8%E3%83%91%E3%83%96%E3%83%AA%E3%83%B3%E3%82%B0%E3%83%95%E3%82%A7%E3%83%BC%E3%82%BA%E3%82%92%E7%AC%AC3%E5%BC%95%E6%95%B0%E3%81%A7%E6%8C%87%E5%AE%9A
+// ダブルクリック用フラグ．区別のためにchar
+let clickFlag = "none";
+
+// クリックとダブルクリックのイベント
 preset01.addEventListener('click', function (e) {
-    // パネルレイアウトをプリセット1のレイアウトにする
-    // panelInfo = data01;
-    alert('preset01');
+    // クリックフラグが立っている状態でのクリック
+    //     -> ダブルクリック
+    if (clickFlag == "panel01") {
+        alert("panel01 double click!!");
+
+        clickFlag = false;
+        return;
+    }
+
+    // シングルクリックを受理、300ms間だけダブルクリック判定を残す
+    clickFlag = "panel01";
+    setTimeout(function () {
+        // ダブルクリックによりclickedフラグがリセットされていない
+        //     -> シングルクリックだった
+
+        // パネルレイアウトをプリセット1のレイアウトにする
+        // panelInfo = data01;
+        if (clickFlag == "panel01") {
+            alert("panel01 single click!");
+        }
+
+        clickFlag = "none";
+    }, 300);
+
 }, false);
+
 preset02.addEventListener('click', function (e) {
-    // パネルレイアウトをプリセット2のレイアウトにする
-    // panelInfo = data02;
-    alert('preset02');
+    // クリックフラグが立っている状態でのクリック
+    //     -> ダブルクリック
+    if (clickFlag == "panel02") {
+        alert("panel02 double click!!");
+
+        clickFlag = false;
+        return;
+    }
+
+    // シングルクリックを受理、300ms間だけダブルクリック判定を残す
+    clickFlag = "panel02";
+    setTimeout(function () {
+        // ダブルクリックによりclickedフラグがリセットされていない
+        //     -> シングルクリックだった
+
+        // パネルレイアウトをプリセット2のレイアウトにする
+        // panelInfo = data02;
+        if (clickFlag == "panel02") {
+            alert("panel02 single click!");
+        }
+
+        clickFlag = "none";
+    }, 300);
+
 }, false);
+
 preset03.addEventListener('click', function (e) {
-    // パネルレイアウトをプリセット3のレイアウトにする
-    // panelInfo = data03;
-    alert('preset03');
+    // クリックフラグが立っている状態でのクリック
+    //     -> ダブルクリック
+    if (clickFlag == "panel03") {
+        alert("panel03 double click!!");
+
+        clickFlag = false;
+        return;
+    }
+
+    // シングルクリックを受理、300ms間だけダブルクリック判定を残す
+    clickFlag = "panel03";
+    setTimeout(function () {
+        // ダブルクリックによりclickedフラグがリセットされていない
+        //     -> シングルクリックだった
+
+        // パネルレイアウトをプリセット3のレイアウトにする
+        // panelInfo = data03;
+        if (clickFlag == "panel03") {
+            alert("panel03 single click!");
+        }
+
+        clickFlag = "none";
+    }, 300);
+
 }, false);
+
 preset04.addEventListener('click', function (e) {
-    // パネルレイアウトをプリセット4のレイアウトにする
-    // panelInfo = data04;
-    alert('preset04');
+    // クリックフラグが立っている状態でのクリック
+    //     -> ダブルクリック
+    if (clickFlag == "panel04") {
+        alert("panel04 double click!!");
+
+        clickFlag = false;
+        return;
+    }
+
+    // シングルクリックを受理、300ms間だけダブルクリック判定を残す
+    clickFlag = "panel04";
+    setTimeout(function () {
+        // ダブルクリックによりclickedフラグがリセットされていない
+        //     -> シングルクリックだった
+
+        // パネルレイアウトをプリセット2のレイアウトにする
+        // panelInfo = data04;
+        if (clickFlag == "panel04") {
+            alert("panel04 single click!");
+        }
+
+        clickFlag = "none";
+    }, 300);
+
 }, false);
