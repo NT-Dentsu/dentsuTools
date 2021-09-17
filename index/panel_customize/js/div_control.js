53229cf80650677acae0f1e0167c5a0bbdecd181
bot810 <kutsuhimo197@gmail.com>
2021-09-17 23:50:30 +0900

    パネル配置部分を関数化と，パネル消去関数を追加．

--- index/panel_customize/js/div_control.js
+++ index/panel_customize/js/div_control.js
@@ -15,7 +15,10 @@ alert(panelInfo)
 // panelInfoはpanel_dataset_costomize.jsで定義されている
 // イベントを使ってクリック時の動作を制御する
 // イベント設定のためname属性にpanelを指定
-panelInfo.forEach(function (info) {
+panelInfo.forEach(containerAppend);
+
+// conttainerに子要素を追加する
+function containerAppend(info){
     text = `
     <div class="${info.className}" style="grid-area: ${info.gridSize};">
         <img src="./images/${info.iamgeLink}" alt="" class="panelImg">
@@ -23,4 +26,19 @@ panelInfo.forEach(function (info) {
     `;
 
     id.append(text);
-});
+}
+
+// containerの子要素を消す(更新処理用)
+function containerEnpty(){
+    // 子要素消去
+    id.empty();
+
+    // 注釈と調整を追加(調整は必須)
+    text = `
+    <!-- グリッドのサイズ調整用 -->
+    <div class="panel-wrap"></div>
+
+    <!-- ここにパネルが追加される -->
+    `;
+    id.append(text);
+}
