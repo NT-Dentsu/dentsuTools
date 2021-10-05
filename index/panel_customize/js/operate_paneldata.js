32413188a2148667b4914c3d7affd4a278e0a2ef
towa1204 <fdsfgjkal@gmail.com>
2021-10-06 00:00:50 +0900

    panel_nameを実装する関数を実装

--- index/panel_customize/js/operate_paneldata.js
+++ index/panel_customize/js/operate_paneldata.js
@@ -1,15 +1,11 @@
 $(function(){
     $('#get_data').click(function(e) {
-
-        getUserPanels().done(function(data){
-            // get_user_panels.phpの結果がdataに格納される
+        getPanelName().done(function(data){
             console.log(data);
-            // 以下にdataを取得した後の処理を結果
         }).fail(function(XMLHttpRequest, textStatus, errorThrown){
             // ajax通信の処理に失敗したとき
             alert(errorThrown);
         });
-
     })
 })
 
@@ -18,6 +14,15 @@ $(function(){
 function getUserPanels() {
     return $.get({
         url : "/panel_customize/php/get_user_panels.php",
+        dataType : 'json'
+    });
+}
+
+// m_panelテーブルに格納されているpanel_nameを取り出す関数
+// JSONデータの形式:{status：実行結果のステータス, array：パネルネームの配列}
+function getPanelName() {
+    return $.get({
+        url : "/panel_customize/php/get_panel_name.php",
         dataType : 'json'
     });
 }
---
+++ index/panel_customize/php/get_panel_name.php
@@ -0,0 +1,42 @@
+<?php
+    require_once __DIR__ . '/../../common/dbaccess.php';
+    //ヘッダー情報の明記。必須。
+    header("Content-Type: application/json; charset=UTF-8");
+
+    // // デバッグ用の処理
+    // error_reporting(E_ALL);
+    // ini_set('display_errors', 'On');
+
+    // phpの実行結果のステータス
+    const STATUS_OK = "OK";
+    const STATUS_NG = "NG";
+
+    // 連想配列の配列の初期化
+    $resultArray = array();
+    // ステータスを格納するための変数
+    $status = STATUS_NG;
+
+    try {
+        $db = new DBAccess();
+        // データベース接続開始
+        $pdo = $db->connectDB();
+
+        // panel_nameを取り出すSQL文
+        $sql = "SELECT panel_name FROM m_panel";
+        $stmt = $pdo->query($sql);
+
+        while ($buff = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $resultArray[] = $buff['panel_name'];
+        }
+        $status = STATUS_OK;
+    } catch (Exception $ex) {
+        $status = STATUS_NG;
+    }
+
+    // JSONの出力
+    $result = array(
+        "status" => $status,
+        "array"  => $resultArray
+    );
+    echo json_encode($result);
+?>
