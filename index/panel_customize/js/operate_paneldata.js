b689526a63e5bd35f97ad05dca74bef0d2740542
towa1204 <fdsfgjkal@gmail.com>
2021-10-09 11:53:31 +0900

    updateの処理を追加

--- index/panel_customize/js/operate_paneldata.js
+++ index/panel_customize/js/operate_paneldata.js
@@ -1,16 +1,5 @@
-$(function(){
-    $('#get_data').click(function(e) {
-        getPanelName().done(function(data){
-            console.log(data);
-        }).fail(function(XMLHttpRequest, textStatus, errorThrown){
-            // ajax通信の処理に失敗したとき
-            alert(errorThrown);
-        });
-    })
-})
-
 // セッション変数に格納されているユーザ名のパネルデータを取得し返す関数
-// JSONデータの形式：{status: 実行結果のステータス, array: パネルデータ(連想配列の配列)}
+// 戻り値JSONデータの形式：{status: 実行結果のステータス, array: パネルデータ(連想配列の配列)}
 function getUserPanels() {
     return $.get({
         url : "/panel_customize/php/get_user_panels.php",
@@ -18,8 +7,22 @@ function getUserPanels() {
     });
 }
 
+// ユーザパネルのデータを渡しDBに反映させる関数
+// 0~15のアンカー番号のうち指定されていないものは、パネルデータ・パネルサイズをNULLにする
+// 渡す変数dictの形式：連想配列を要素とする配列
+function updateUserPanels(dict) {
+    return $.post({
+        url : "/panel_customize/php/update_user_panels.php",
+        dataType : 'json',
+        contentType : "application/json",
+        data : JSON.stringify({
+            panelData : dict
+        })
+    });
+}
+
 // m_panelテーブルに格納されているpanel_nameを取り出す関数
-// JSONデータの形式:{status：実行結果のステータス, array：パネルネームの配列}
+// 戻り値JSONデータの形式:{status：実行結果のステータス, array：パネルネームの配列}
 function getPanelName() {
     return $.get({
         url : "/panel_customize/php/get_panel_name.php",
