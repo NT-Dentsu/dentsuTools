12768890fe35eaaff391ad95fe93ee63af244adc
towa1204 <fdsfgjkal@gmail.com>
2021-11-27 14:51:36 +0900

    URLを相対ルートパスに変更

--- doc/DB/QUERY/insert_preset_data.sql
+++ doc/DB/QUERY/insert_preset_data.sql
@@ -22,7 +22,7 @@ insert into t_user_panel_info values ("preset001", NULL, 14, NULL);
 insert into t_user_panel_info values ("preset001", "size S", 15, 5);
 
 # preset001に使うパネル(m_panel)の作成(絶対パスのほうが良さそう)
-insert into m_panel values ("size L", "./app/kindle_copy/kindle_copy.php", "./images/image_panel_L.jpg");
-insert into m_panel values ("size M hol", "./app/kindle_copy/kindle_copy.php", "./images/image_panel_M_h.jpg");
-insert into m_panel values ("size M var", "./app/kindle_copy/kindle_copy.php", "./images/image_panel_M_v.jpg");
-insert into m_panel values ("size S", "./app/kindle_copy/kindle_copy.php", "./images/image_panel_S.jpg");
+insert into m_panel values ("size L", "/app/kindle_copy/kindle_copy.php", "/images/image_panel_L.jpg");
+insert into m_panel values ("size M hol", "/app/kindle_copy/kindle_copy.php", "/images/image_panel_M_h.jpg");
+insert into m_panel values ("size M var", "/app/kindle_copy/kindle_copy.php", "/images/image_panel_M_v.jpg");
+insert into m_panel values ("size S", "/app/kindle_copy/kindle_copy.php", "/images/image_panel_S.jpg");
