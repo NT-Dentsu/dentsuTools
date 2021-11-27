9d79856fe4f098fa9a76b476a51db93cd5e52b9b
bot810 <kutsuhimo197@gmail.com>
2021-11-27 18:52:01 +0900

    更新時サイズ調整用のコードが追加されるようにした

--- index/panel_customize/js/div_control.js
+++ index/panel_customize/js/div_control.js
@@ -1,6 +1,6 @@
 /**
  * 制作者：bot810
- * 更新日：2021/09/18
+ * 更新日：2021/11/27
  * div要素の追加，変更をする．
  */
 
@@ -38,7 +38,13 @@ function containerEnpty(){
     // 注釈と調整を追加(調整は必須)
     text = `
     <!-- グリッドのサイズ調整用 -->
-    <div class="panel-wrap"></div>
+    <div class="panel-wrap" style="grid-area: 1 / 1;"></div>
+    <div class="panel-wrap" style="grid-area: 1 / 2;"></div>
+    <div class="panel-wrap" style="grid-area: 1 / 3;"></div>
+    <div class="panel-wrap" style="grid-area: 1 / 4;"></div>
+    <div class="panel-wrap" style="grid-area: 2 / 1;"></div>
+    <div class="panel-wrap" style="grid-area: 3 / 1;"></div>
+    <div class="panel-wrap" style="grid-area: 4 / 1;"></div>
 
     <!-- ここにパネルが追加される -->
     `;
