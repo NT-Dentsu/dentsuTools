757deeabafe977f5948ff7c2fbe1429751d7c810
KARASU-2000 <KARASU-2000>
2021-12-11 14:56:48 +0900

    close #133 Createのスクリプト修正(utf8→utf8mb4)

--- doc/DB/QUERY/create_m_user.sql
+++ doc/DB/QUERY/create_m_user.sql
@@ -1,15 +1,16 @@
+#Rev 1.3 utf8→utf8mb4に修正
 #Rev 1.2 パスワードの型をVARCHARからCHARに変更
 #Rev 1.1 テーブル名の前にDB名を追加
 #        user_iconの型をVARCHARからTEXTに変更(全てのカラムの文字数が65535を超えてエラーが発生するため)
 #Rev 1.0 新規作成
 
 CREATE TABLE IF NOT EXISTS dentsudb.m_user(
-    user_id VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-    user_name VARCHAR(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-    password_hash CHAR(60) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+    user_id VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+    user_name VARCHAR(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+    password_hash CHAR(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
     insert_time DATETIME,
     update_time DATETIME,
-    user_icon TEXT CHARACTER SET utf8 COLLATE utf8_bin,
+    user_icon TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
     PRIMARY KEY(
         user_id
     )
--- doc/DB/QUERY/create_t_user_panel_info.sql
+++ doc/DB/QUERY/create_t_user_panel_info.sql
@@ -1,11 +1,12 @@
+#Rev 1.4 utf8→utf8mb4に修正
 #Rev 1.3 panel_nameとpanel_sizeのNULLを許可、主キーを変更
 #Rev 1.2 panel_sizeカラムのデフォルト値を設定
 #Rev 1.1 テーブル名の前にDB名を追加
 #Rev 1.0 新規作成
 
 CREATE TABLE IF NOT EXISTS dentsudb.t_user_panel_info(
-    user_id VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-    panel_name VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_bin,
+    user_id VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+    panel_name VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
     anchor_num SMALLINT UNSIGNED NOT NULL,
     panel_size TINYINT UNSIGNED DEFAULT 5,
     PRIMARY KEY(
