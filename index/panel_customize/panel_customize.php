a7414b6f6e53a7d615ccf813a3f9debd8ebec33f
bot810 <kutsuhimo197@gmail.com>
2022-02-26 21:36:50 +0900

    グリッドサイズ調整兼インデックス取得用のdiv要素追加

--- index/panel_customize/panel_customize.php
+++ index/panel_customize/panel_customize.php
@@ -48,10 +48,26 @@
                         <!-- 「id=”container”」という親要素（div 要素）内に配置した「class=”panel_wrap”」という要素（div 要素）を整列させる処理を行う -->
                         <div id="wrapper">
                             <div id="container">
-                                <!-- グリッドのサイズ調整用 -->
-                                <div class="panel-wrap"></div>
+                                <!-- グリッドのサイズ調整用兼ポジション取得用 -->
+                                <div class="panel-wrap" name="0" style="grid-area: 1 / 1;"></div>
+                                <div class="panel-wrap" name="1" style="grid-area: 1 / 2;"></div>
+                                <div class="panel-wrap" name="2" style="grid-area: 1 / 3;"></div>
+                                <div class="panel-wrap" name="3" style="grid-area: 1 / 4;"></div>
+                                <div class="panel-wrap" name="4" style="grid-area: 2 / 1;"></div>
+                                <div class="panel-wrap" name="5" style="grid-area: 2 / 2;"></div>
+                                <div class="panel-wrap" name="6" style="grid-area: 2 / 3;"></div>
+                                <div class="panel-wrap" name="7" style="grid-area: 2 / 4;"></div>
+                                <div class="panel-wrap" name="8" style="grid-area: 3 / 1;"></div>
+                                <div class="panel-wrap" name="9" style="grid-area: 3 / 2;"></div>
+                                <div class="panel-wrap" name="10" style="grid-area: 3 / 3;"></div>
+                                <div class="panel-wrap" name="11" style="grid-area: 3 / 4;"></div>
+                                <div class="panel-wrap" name="12" style="grid-area: 4 / 1;"></div>
+                                <div class="panel-wrap" name="13" style="grid-area: 4 / 2;"></div>
+                                <div class="panel-wrap" name="14" style="grid-area: 4 / 3;"></div>
+                                <div class="panel-wrap" name="15" style="grid-area: 4 / 4;"></div>
 
                                 <!-- ここにパネルが追加される -->
+
                             </div>
                         </div>
                     </div>
