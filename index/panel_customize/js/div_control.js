5fb1fa4428dd8def7a595f12eae027d08454b749
bot810 <kutsuhimo197@gmail.com>
2022-02-26 21:38:53 +0900

    パネル調整兼インデックス取得用のdivを追加する様に変更

--- index/panel_customize/js/div_control.js
+++ index/panel_customize/js/div_control.js
@@ -38,13 +38,22 @@ function containerEnpty(){
     // 注釈と調整を追加(調整は必須)
     text = `
     <!-- グリッドのサイズ調整用 -->
-    <div class="panel-wrap" style="grid-area: 1 / 1;"></div>
-    <div class="panel-wrap" style="grid-area: 1 / 2;"></div>
-    <div class="panel-wrap" style="grid-area: 1 / 3;"></div>
-    <div class="panel-wrap" style="grid-area: 1 / 4;"></div>
-    <div class="panel-wrap" style="grid-area: 2 / 1;"></div>
-    <div class="panel-wrap" style="grid-area: 3 / 1;"></div>
-    <div class="panel-wrap" style="grid-area: 4 / 1;"></div>
+    <div class="panel-wrap" name="0" style="grid-area: 1 / 1;"></div>
+    <div class="panel-wrap" name="1" style="grid-area: 1 / 2;"></div>
+    <div class="panel-wrap" name="2" style="grid-area: 1 / 3;"></div>
+    <div class="panel-wrap" name="3" style="grid-area: 1 / 4;"></div>
+    <div class="panel-wrap" name="4" style="grid-area: 2 / 1;"></div>
+    <div class="panel-wrap" name="5" style="grid-area: 2 / 2;"></div>
+    <div class="panel-wrap" name="6" style="grid-area: 2 / 3;"></div>
+    <div class="panel-wrap" name="7" style="grid-area: 2 / 4;"></div>
+    <div class="panel-wrap" name="8" style="grid-area: 3 / 1;"></div>
+    <div class="panel-wrap" name="9" style="grid-area: 3 / 2;"></div>
+    <div class="panel-wrap" name="10" style="grid-area: 3 / 3;"></div>
+    <div class="panel-wrap" name="11" style="grid-area: 3 / 4;"></div>
+    <div class="panel-wrap" name="12" style="grid-area: 4 / 1;"></div>
+    <div class="panel-wrap" name="13" style="grid-area: 4 / 2;"></div>
+    <div class="panel-wrap" name="14" style="grid-area: 4 / 3;"></div>
+    <div class="panel-wrap" name="15" style="grid-area: 4 / 4;"></div>
 
     <!-- ここにパネルが追加される -->
     `;
