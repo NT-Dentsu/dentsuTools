888019252488ea768e3e14d991f375491f5ff6f6
bot810 <kutsuhimo197@gmail.com>
2022-03-12 19:56:07 +0900

    プリセットデータをデータベースから読み込むように変更

--- index/panel_customize/js/panel_dataset_init_customize.js
+++ index/panel_customize/js/panel_dataset_init_customize.js
@@ -74,54 +74,32 @@ function panelPreset(presetID) {
 }
 
 // プリセットレイアウトの並びを定義
-let data01 = new Array();
-let data02 = new Array();
-let data03 = new Array();
-let data04 = new Array();
-
 // プリセット01
-// (名前, 位置, 大きさ, 画像, ツールへのリンク)
-data01.push(new PanelInfo("size L", 4, 2, "/images/image_panel_L.jpg", "/panel_sample/panel_sample.html"));
-data01.push(new PanelInfo("size M hol", 0, 4, "/images/image_panel_M_h.jpg", "/panel_sample/panel_sample.html"));
-data01.push(new PanelInfo("size M var", 3, 3, "/images/image_panel_M_v.jpg", "/panel_sample/panel_sample.html"));
-data01.push(new PanelInfo("size M var", 6, 3, "/images/image_panel_M_v.jpg", "/panel_sample/panel_sample.html"));
-data01.push(new PanelInfo("size M hol", 13, 4, "/images/image_panel_M_h.jpg", "/panel_sample/panel_sample.html"));
-data01.push(new PanelInfo("size S", 2, 5, "/images/image_panel_S.jpg", "/panel_sample/panel_sample.html"));
-data01.push(new PanelInfo("size S", 11, 5, "/images/image_panel_S.jpg", "/panel_sample/panel_sample.html"));
-data01.push(new PanelInfo("size S", 12, 5, "/images/image_panel_S.jpg", "/panel_sample/panel_sample.html"));
-data01.push(new PanelInfo("size S", 15, 5, "/images/image_panel_S.jpg", "/panel_sample/panel_sample.html"));
-
+let data01;
+new Promise((resolve) => {
+    resolve(panelPreset("preset001"));
+}).then((data) => {
+    data01 = data;
+});
 // プリセット02
-// (名前, 位置, 大きさ, 画像, ツールへのリンク)
-data02.push(new PanelInfo("size L", 5, 2, "/images/image_panel_L.jpg", "/panel_sample/panel_sample.html"));
-data02.push(new PanelInfo("size M hol", 1, 4, "/images/image_panel_M_h.jpg", "/panel_sample/panel_sample.html"));
-data02.push(new PanelInfo("size M hol", 13, 4, "/images/image_panel_M_v.jpg", "/panel_sample/panel_sample.html"));
-data02.push(new PanelInfo("size M var", 4, 3, "/images/image_panel_M_v.jpg", "/panel_sample/panel_sample.html"));
-data02.push(new PanelInfo("size M var", 7, 3, "/images/image_panel_M_h.jpg", "/panel_sample/panel_sample.html"));
-data02.push(new PanelInfo("size S", 0, 5, "/images/image_panel_S.jpg", "/panel_sample/panel_sample.html"));
-data02.push(new PanelInfo("size S", 3, 5, "/images/image_panel_S.jpg", "/panel_sample/panel_sample.html"));
-data02.push(new PanelInfo("size S", 12, 5, "/images/image_panel_S.jpg", "/panel_sample/panel_sample.html"));
-data02.push(new PanelInfo("size S", 15, 5, "/images/image_panel_S.jpg", "/panel_sample/panel_sample.html"));
-
+let data02;
+new Promise((resolve) => {
+    resolve(panelPreset("preset002"));
+}).then((data) => {
+    data02 = data;
+});
 // プリセット03
-// (名前, 位置, 大きさ, 画像, ツールへのリンク)
-data03.push(new PanelInfo("size L", 2, 2, "/images/image_panel_L.jpg", "/panel_sample/panel_sample.html"));
-data03.push(new PanelInfo("size L", 8, 2, "/images/image_panel_M_h.jpg", "/panel_sample/panel_sample.html"));
-data03.push(new PanelInfo("size M var", 0, 3, "/images/image_panel_M_v.jpg", "/panel_sample/panel_sample.html"));
-data03.push(new PanelInfo("size M var", 11, 3, "/images/image_panel_M_h.jpg", "/panel_sample/panel_sample.html"));
-data03.push(new PanelInfo("size S", 1, 5, "/images/image_panel_S.jpg", "/panel_sample/panel_sample.html"));
-data03.push(new PanelInfo("size S", 5, 5, "/images/image_panel_S.jpg", "/panel_sample/panel_sample.html"));
-data03.push(new PanelInfo("size S", 10, 5, "/images/image_panel_S.jpg", "/panel_sample/panel_sample.html"));
-data03.push(new PanelInfo("size S", 14, 5, "/images/image_panel_S.jpg", "/panel_sample/panel_sample.html"));
-
+let data03;
+new Promise((resolve) => {
+    resolve(panelPreset("preset003"));
+}).then((data) => {
+    data03 = data;
+});
 // プリセット04
-// (名前, 位置, 大きさ, 画像, ツールへのリンク)
-data04.push(new PanelInfo("size L", 0, 2, "/images/image_panel_L.jpg", "/panel_sample/panel_sample.html"));
-data04.push(new PanelInfo("size M hol", 2, 4, "/images/image_panel_M_h.jpg", "/panel_sample/panel_sample.html"));
-data04.push(new PanelInfo("size M hol", 13, 4, "/images/image_panel_M_v.jpg", "/panel_sample/panel_sample.html"));
-data04.push(new PanelInfo("size M var", 7, 3, "/images/image_panel_M_v.jpg", "/panel_sample/panel_sample.html"));
-data04.push(new PanelInfo("size M var", 8, 3, "/images/image_panel_M_h.jpg", "/panel_sample/panel_sample.html"));
-data04.push(new PanelInfo("size S", 6, 5, "/images/image_panel_S.jpg", "/panel_sample/panel_sample.html"));
-data04.push(new PanelInfo("size S", 9, 5, "/images/image_panel_S.jpg", "/panel_sample/panel_sample.html"));
-data04.push(new PanelInfo("size S", 10, 5, "/images/image_panel_S.jpg", "/panel_sample/panel_sample.html"));
-data04.push(new PanelInfo("size S", 15, 5, "/images/image_panel_S.jpg", "/panel_sample/panel_sample.html"));
+let data04;
+new Promise((resolve) => {
+    resolve(panelPreset("preset004"));
+}).then((data) => {
+    data04 = data;
+});
+
