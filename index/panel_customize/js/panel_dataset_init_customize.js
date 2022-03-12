33c2a03a1ff5b34c69477c7ea7f3fd1b48d3159f
bot810 <kutsuhimo197@gmail.com>
2022-03-12 19:50:40 +0900

    プリセットデータ読み込み用の関数

--- index/panel_customize/js/panel_dataset_init_customize.js
+++ index/panel_customize/js/panel_dataset_init_customize.js
@@ -45,7 +45,32 @@ function panelInit() {
     });
 }
 
+// データベースから指定したプリセットパネルデータを読み込んみ，表示用に成型して返り値に渡す
+function panelPreset(presetID) {
+    // 同期処理
+    return get_presetdata(presetID)
+    .then((data) => {
+        // パネルデータをデータベースから取得
+        console.log(data.status);
+        console.log(data.array);
+        return data.array;
+    })
+    .then((panelData) => { // 上の処理が終わった後に実行
+        // 渡すデータ
+        let panelInfo = new Array();
+        // パネルデータをもとにパネルクラス作成
+        panelData.forEach(data => {
+            // 連想配列の形式(要素は順不同)
+            // {"panel_name" : <String>, "anchor_num" : <int>, "panel_size" : <int>, "content_link" : <String>, "content_image" : <String>}
+            // PanelInfoの引数
+            // (名前, 位置, 大きさ, 画像, ツールへのリンク)
+            panelInfo.push(new PanelInfo(data.panel_name, data.anchor_num, data.panel_size, data.content_image, data.content_link));
+        });
 
+        // 最終的にここの値が返る
+        return panelInfo;
+    });
+}
 
 // プリセットレイアウトの並びを定義
 let data01 = new Array();
