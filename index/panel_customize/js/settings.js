9e848c56acbeb1e4cde58d4301e9a9f5765ae9d5
bot810 <kutsuhimo197@gmail.com>
2022-04-09 15:10:11 +0900

    masonry使用部分削除

--- index/panel_customize/js/settings.js
+++ index/panel_customize/js/settings.js
@@ -1,25 +1,9 @@
 /**
  * 制作者：bot810
- * 更新日：2022/02/26
+ * 更新日：2022/04/09
  * こまごました設定ファイル．後で纏める可能性大
  */
 
-//画像読み込み後に並べ替えるようにする
-// jQuery.event.add(window, "load", function () {
-//     //ここに Masonry プラグインのイニシャライズやオプションを記述
-//     //（別ファイルに記述してもOK。そのファイルを読み込む必要あり）
-//     //jQuery プラグイン Masonry のイニシャライズを参照
-//     $('#container').masonry({
-//         itemSelector: '.panel-wrap',
-//         // 要素の横の間隔(最低値)
-//         columnWidth: 0,
-//         isFitWidth: false,  //親要素の幅をパネル幅に合わせる
-//         //並び順を入れ替えない
-//         horizontalOrder: true
-//         //なんか表示をパーセントで
-//         //percentPosition: true
-//     });
-// });
 
 //メニューバーのアイコンを表示する
 //参考
